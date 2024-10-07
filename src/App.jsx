5eab1c575a73701c24306800043bcbefaa1d0118
unknown <amananurag.20@gmail.com>
2024-10-07 10:33:19 +0530

    get request

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,26 @@
-import React from 'react'
+
+import { useEffect, useState } from 'react'
 import CreateTodo from './components/CreateTodo'
 import Todos from './components/Todos'
 
 const App = () => {
-  const data=[
-    {
-      _id: "66ff727ccf08fd7e1526ca82",
-      title: "1211",
-      description: "we are learninig backend in express",
-      completed: false,
-      __v: 0
-    },
-    {
-      _id: "66ff7334cf08fd7e1526ca85",
-      title: "1211",
-      description: "we are learninig backend in express",
-      completed: false,
-      __v: 0
-    }]
+   
+  const [todos, setTodos]= useState([]);
+
+  const fetchTodos=async()=>{
+    const response= await fetch("http://localhost:5000/todos");
+    const data= await response.json();
+    setTodos(data.todos);  //infinite loop
+  }
+    
+  useEffect(()=>{
+    fetchTodos(); 
+  },[]);
+
   return (
     <div>
       <CreateTodo/>
-      <Todos todos={data}/>
+      <Todos todos={todos}/>
     </div>
   )
 }
